23c9b319545e80abf07c802b966bca9b0a27d426
Robert Zhu <robzhu@fb.com>
2018-01-18 17:35:35 -0500

    initial checkin

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(80, () => console.log('web-starter running on port 80'));
